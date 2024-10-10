951048ac0d1b934ec806033b36a19b3c99debd24
Mateo Ismael <mateoismaelrr5@gmail.com>
2024-10-10 13:41:35 -0500

    primer commit

---
+++ src/test/java/com/example/fammz/FammzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fammz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FammzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
