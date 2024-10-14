b475f0fa2a2fa1d13b1344f398f113119c300229
Mateo Ismael <mateoismaelrr5@gmail.com>
2024-10-13 19:01:51 -0500

    ajustes

--- src/main/java/com/example/fammz/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.fammz.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
